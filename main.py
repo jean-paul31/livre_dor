9e547079dd5c753a7b2b13f526364a89d7ee8f73
jean-paul31 <andrei.jpaul@gmail.com>
2024-06-16 23:50:19 +0200

    ok

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ textes = {
     "timer_format": "{:02d}:{:02d}",
     "francais": "Français",
     "portugais": "Português",
+    "fichier_existe": "Le fichier existe déjà. Veuillez choisir un autre nom."
 }
 
 def traduire_en_francais():
@@ -47,6 +48,7 @@ def traduire_en_francais():
         "timer_format": "{:02d}:{:02d}",
         "francais": "Français",
         "portugais": "Portugês",
+        "fichier_existe": "Veuillez choisir un autre nom."
     }
     mettre_a_jour_interface()
 
@@ -70,45 +72,51 @@ def traduire_en_portugais():
         "timer_format": "{:02d}:{:02d}",
         "francais": "Francês",
         "portugais": "Portugês",
+        "fichier_existe": "Por favor, escolha outro nome."
     }
     mettre_a_jour_interface()
 
 def mettre_a_jour_interface():
-    # Mettre à jour les textes des widgets avec les nouvelles traductions
     bouton_commencer.config(text=textes["commencer"])
     bouton_arreter.config(text=textes["arreter"])
-    label_message.config(text=textes["enregistrement_en_cours"])
+    label_message.config(text="")
     label_resultat.config(text=textes["resultat"])
     label_timer.config(textvariable=variable_timer)
 
-    # Mettre à jour les instructions
     text_instructions.config(state=tk.NORMAL)
     text_instructions.delete(1.0, tk.END)
     text_instructions.insert(tk.END, textes["instructions"])
     text_instructions.config(state=tk.DISABLED)
 
+def fichier_existe_deja(nom_fichier):
+    dossier_audio = "audio"
+    fichier_audio = os.path.join(dossier_audio, nom_fichier + ".wav")
+    return os.path.isfile(fichier_audio)
+
 def commencer_enregistrement():
     global enregistrement_en_cours, enregistrement, thread_recording, timer_thread, start_time
     if not enregistrement_en_cours:
+        name = champ_saisie.get()
+        if fichier_existe_deja(name):
+            label_message.config(text=textes["fichier_existe"], fg='red')
+            return
+        
         enregistrement_en_cours = True
-        label_message.config(text=textes["enregistrement_en_cours"])
+        label_message.config(text=textes["enregistrement_en_cours"], fg='black')
         bouton_commencer.config(state=tk.DISABLED)
         bouton_arreter.config(state=tk.NORMAL)
 
-        name = afficher_saisie()
         global fichier_audio
-        dossier_audio = "audio"  # Nom du dossier
-        if not os.path.exists(dossier_audio):  # Vérifier si le dossier existe, sinon le créer
+        dossier_audio = "audio"
+        if not os.path.exists(dossier_audio):
             os.makedirs(dossier_audio)
-        fichier_audio = os.path.join(dossier_audio, name + ".wav")  # Chemin complet du fichier audio
+        fichier_audio = os.path.join(dossier_audio, name + ".wav")
 
-        # Démarrer le thread d'enregistrement
-        start_time = time.time()  # Capturer le temps de début
+        start_time = time.time()
         enregistrement = []
         thread_recording = threading.Thread(target=record_audio)
         thread_recording.start()
 
-        # Démarrer le compte à rebours dans un thread séparé
         timer_thread = threading.Thread(target=compte_a_rebours, args=(3 * 60,))
         timer_thread.start()
 
@@ -118,8 +126,7 @@ def record_audio():
         while enregistrement_en_cours:
             enregistrement.append(stream.read(1024)[0])
             sd.sleep(10)
-    enregistrement = np.concatenate(enregistrement, axis=0)  # Convertir la liste en tableau NumPy
-
+    enregistrement = np.concatenate(enregistrement, axis=0)
 
 def arreter_enregistrement():
     global enregistrement_en_cours, timer_thread
@@ -129,18 +136,15 @@ def arreter_enregistrement():
         bouton_commencer.config(state=tk.NORMAL)
         bouton_arreter.config(state=tk.DISABLED)
 
-        # Convertir la liste en un tableau NumPy
         enregistrement_array = np.concatenate(enregistrement, axis=0)
 
-        duration = time.time() - start_time  # Calculer la durée réelle
-        if duration < 3 * 60:  # Si arrêté manuellement
+        duration = time.time() - start_time
+        if duration < 3 * 60:
             wavio.write(fichier_audio, enregistrement_array[:int(duration * 44100)], 44100, sampwidth=3)
-        else:  # Si arrêté automatiquement après 3 minutes
+        else:
             wavio.write(fichier_audio, enregistrement_array, 44100, sampwidth=3)
 
-        champ_saisie.delete(0, tk.END)  # Effacer le contenu du champ de saisie
-
-        # Réinitialiser le compte à rebours
+        champ_saisie.delete(0, tk.END)
         variable_timer.set(textes["timer_format"].format(3, 0))
 
 def afficher_saisie():
@@ -156,59 +160,47 @@ def compte_a_rebours(duree):
         time.sleep(1)
         duree -= 1
 
-    if enregistrement_en_cours:  # Arrêter l'enregistrement automatiquement à la fin du compte à rebours
+    if enregistrement_en_cours:
         arreter_enregistrement()
 
-# Interface graphique
 fenetre = tk.Tk()
 fenetre.title("Enregistreur Audio")
 fenetre.geometry("1000x500")
 
-# Créer un frame pour les widgets de contrôle
 frame_controls = tk.Frame(fenetre)
 frame_controls.pack(side=tk.LEFT, padx=20, pady=20)
 
-# Variable pour le statut de l'enregistrement
 enregistrement_en_cours = False
 enregistrement = None
 start_time = None
 
-# Widget Entry pour la saisie de texte
 champ_saisie = tk.Entry(frame_controls, font=('Arial', 16))
 champ_saisie.pack(pady=10)
 
-# Bouton pour commencer l'enregistrement
 bouton_commencer = tk.Button(frame_controls, command=commencer_enregistrement)
 bouton_commencer.config(font=('Arial', 14), background='#4CAF50', text=textes["commencer"], foreground='white', padx=20, pady=10)
 bouton_commencer.pack(pady=10)
 
-# Bouton pour arrêter l'enregistrement
 bouton_arreter = tk.Button(frame_controls, command=arreter_enregistrement, state=tk.DISABLED)
 bouton_arreter.config(font=('Arial', 14), text=textes["arreter"], background='#FF0000', foreground='white', padx=20, pady=10)
 bouton_arreter.pack(pady=10)
 
-# Label pour afficher le message d'enregistrement
 label_message = tk.Label(frame_controls, font=('Arial', 12))
 label_message.pack(pady=10)
 
-# Label pour afficher le résultat
 label_resultat = tk.Label(frame_controls, font=('Arial', 14))
 label_resultat.pack(pady=10)
 
-# Label pour afficher le compte à rebours
 variable_timer = tk.StringVar(value="03:00")
 label_timer = tk.Label(frame_controls, textvariable=variable_timer, font=('Arial', 24))
 label_timer.pack(pady=10)
 
-# Créer un frame pour les instructions et les boutons de traduction
 frame_instructions = tk.Frame(fenetre)
 frame_instructions.pack(side=tk.RIGHT, padx=20, pady=20)
 
-# Ajouter les boutons de traduction
 frame_buttons = tk.Frame(frame_instructions)
 frame_buttons.pack(pady=10)
 
-# Charger les images des drapeaux
 image_francais = Image.open("assets/Flag_of_France.svg.png")
 image_francais = image_francais.resize((50, 30), Image.LANCZOS)
 photo_francais = ImageTk.PhotoImage(image_francais)
@@ -217,24 +209,20 @@ image_portugais = Image.open("assets/Flag_of_Portugal.svg.png")
 image_portugais = image_portugais.resize((50, 30), Image.LANCZOS)
 photo_portugais = ImageTk.PhotoImage(image_portugais)
 
-# Boutons avec les images des drapeaux
 bouton_francais = tk.Button(frame_buttons, image=photo_francais, command=traduire_en_francais)
 bouton_francais.pack(side=tk.LEFT, padx=5)
 
 bouton_portugais = tk.Button(frame_buttons, image=photo_portugais, command=traduire_en_portugais)
 bouton_portugais.pack(side=tk.LEFT, padx=5)
 
-# Création de la scrollbar
 scrollbar = tk.Scrollbar(frame_instructions)
 scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 
-# Création du Text widget
 text_instructions = tk.Text(frame_instructions, wrap=tk.WORD, yscrollcommand=scrollbar.set, font=('Arial', 12))
 text_instructions.insert(tk.END, textes["instructions"])
-text_instructions.config(state=tk.DISABLED)  # Rendre le Text widget non éditable
+text_instructions.config(state=tk.DISABLED)
 text_instructions.pack(padx=10, pady=10)
 
-# Configurer la scrollbar pour qu'elle contrôle le Text widget
 scrollbar.config(command=text_instructions.yview)
 
 fenetre.mainloop()
